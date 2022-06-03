facf3d943a2f533699b9ac3b98f768daf95848d4
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-06-02 22:16:15 -0400

    Added Index and Show pages (+ controller methods)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,52 @@
 class ProductsController < ApplicationController
 
   def index
+    @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
+    authorize @product
   end
 
   def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    authorize @product
+
+    if @product.save
+      redirect_to root_path(@product), notice: "L'image a bien été créée"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @product.update(product_params)
+    redirect_to product_path(@product), notice: "L'iten a bien été mis à jour"
   end
 
   def destroy
+    @product.destroy
+    redirect_to root_path(@product), notice: "L'item a bien été supprimé"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category, :price, photos: [])
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+    authorize @product
   end
 
 end
